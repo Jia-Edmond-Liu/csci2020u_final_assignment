baa853ae7592f2cf47919eb1a000def2bfa8df0e
DeeNom <jia.liu@uoit,net>
2018-04-01 13:41:56 -0400

    Created and partially completed Server.java

--- src/sample/Server.java
+++ src/sample/Server.java
@@ -6,8 +6,9 @@ import javafx.collections.ObservableList;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Vector;
 
-public class Server extends  Thread{
+public class Server extends Thread{
     private ServerSocket serverSocket;
     private ObservableList<Thread> threads = FXCollections.observableArrayList();
 
@@ -17,18 +18,36 @@ public class Server extends  Thread{
 
     @Override
     public void run() {
-        super.run();
-        while (true){
-            try {
+        try{
+            this.handle();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void quit(){
+        try{
+            serverSocket.close();
+        }
+        catch(IOException e){
+            System.out.println("Closed Socket");
+        }
+    }
+
+    public void handle() throws IOException{
+        try{
+            int clientNum = 0;
+            Vector<Thread> thread = new Vector<Thread>();
+            while(!serverSocket.isClosed()){
                 Socket socket = serverSocket.accept();
-
-                //Thread client = new Thread((Runnable) new Client(socket));
-                //threads.add(client);
-                //client.start();
-
-            } catch (IOException e) {
-                e.printStackTrace();
+                thread.add(clientNum, new Thread(new ClientConnectionHandler(socket)));
+                thread.get(clientNum).start();
+                clientNum++;
+                }
             }
+        catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
