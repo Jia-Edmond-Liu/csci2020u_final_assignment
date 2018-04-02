85baf048787bf01f618266ee1c19a98effe1ba77
DeeNom <jia.liu@uoit,net>
2018-04-02 02:43:38 -0400

    Partially completed SongQueue.java. Worked with Sonika

--- src/sample/SongQueue.java
+++ src/sample/SongQueue.java
@@ -1,9 +1,33 @@
 package sample;
+import java.util.ArrayList;
 
 public class SongQueue {
-    protected static Song[] queuedSongs = new Song[50];
+    protected static ArrayList<String> queuedSongs = new ArrayList<String>();
 
     public SongQueue(){
+        //not sure if constructor is necessary at the moment
+        //nothing to initialize really
+    }
 
+    public String getQueuedSongs(){
+        String output = "";
+        for(int i = 0; i < queuedSongs.size();i++){
+            if(i < queuedSongs.size() - 1){
+                output = queuedSongs.get(i) + ";";
+            }
+            else{
+                output = queuedSongs.get(i);
+            }
+        }
+        return output;
+    }
+
+    public void addSong(Song song){
+        queuedSongs.add(song.getSongName());
+    }
+
+    public void removeSong(Song song){
+        int index = queuedSongs.indexOf(song.getSongName());
+        queuedSongs.remove(index);
     }
 }
